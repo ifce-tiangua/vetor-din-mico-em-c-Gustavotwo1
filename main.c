e3731f3c2570be915fb940ef69d5d13c9833f2d9
Gustavotwo1 <antoniogustavo0211@gmail.com>
2024-11-13 17:17:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,44 @@
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int *vet;
+    int n, i;
+
+    scanf("%d", &n);
+
+    vet = (int)malloc(n*sizeof(int));
+
+    for(i=0; i<n; i++){
+
+        scanf("%d", &*(vet+i));
+
+    }
+    if(n==0){
+
+        printf("[vazio]");
+    }else{
+
+        printf("[");
+
+        for(i=0; i<n; i++){
+
+            if(i==n-1){
+
+                printf("%d", *(vet+i));
+
+            }else{
+
+                printf("%d, ", *(vet+i));
+
+            }
+
+        }
+
+        printf("]");
+
+    }
+
+    free(vet);
 
     return 0;
 }
